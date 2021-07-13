f2c910999bcec4bf8d9592109fcc992c9a919ad3
spences10 <spences10apps@gmail.com>
2021-07-13 19:56:07 +0100

    feat: :sparkles: pass props to component

--- src/lib/toggle.svelte
+++ src/lib/toggle.svelte
@@ -1,7 +1,13 @@
-<label>
-  <input type="checkbox" />
+<script>
+  export let isToggled = false
+  export let label = ''
+  export let style = ''
+</script>
+
+<label {style}>
+  <input type="checkbox" bind:checked={isToggled} />
   <div class="toggle" />
-  Label
+  {label}
 </label>
 
 <style>
@@ -9,6 +15,7 @@
     --width: 50px;
     --height: calc(var(--width) / 2);
     --radius: calc(var(--height) / 2);
+    display: flex;
   }
   .toggle {
     position: relative;
@@ -17,16 +24,18 @@
     border-radius: var(--radius);
     border: solid 1px #333;
     transition: background-color 0.2s ease;
+    margin-right: 5px;
+    background-color: var(--toggleBackgroundColor, white);
   }
   .toggle::after {
     content: '';
     position: absolute;
-    top: 0;
-    left: 0;
+    top: -1px;
+    left: -1px;
     height: var(--height);
     width: var(--height);
     border-radius: var(--radius);
-    background-color: rebeccapurple;
+    background-color: var(--toggleButtonColor, rebeccapurple);
     box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
     transition: transform 0.2s ease;
   }
@@ -34,7 +43,7 @@
     display: none;
   }
   input:checked + .toggle {
-    background-color: papayawhip;
+    background-color: var(--toggleCheckedBackgroundColor, papayawhip);
   }
   input:checked + .toggle::after {
     transform: translate3d(100%, 0, 0);
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,13 @@
 <script>
   import Toggle from '$lib/toggle.svelte'
+  let isToggled = false
 </script>
 
 <h1>Welcome to SvelteKit UI</h1>
 
-<Toggle />
+<Toggle bind:isToggled label="Whee" />
+<Toggle
+  bind:isToggled
+  label="Whee"
+  style="--toggleBackgroundColor: orange"
+/>
