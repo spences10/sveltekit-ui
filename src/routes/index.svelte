f582695ddaa3cd8846c2cbf94047fe5d147b34e9
spences10 <spences10apps@gmail.com>
2021-07-13 21:36:25 +0100

    feat: :sparkles: okish accordian

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,20 @@
 <script>
+  import Accordian from '$lib/accordian.svelte'
   import Toggle from '$lib/toggle.svelte'
   let isToggled = false
 </script>
 
 <h1>Welcome to SvelteKit UI</h1>
 
-<Toggle bind:isToggled label="Whee" />
+<Accordian isOpen={true} buttonText="More info">
+  <div>
+    <p>Here is some information about the accordian</p>
+  </div>
+</Accordian>
+
+<Toggle bind:isToggled label="Default" />
 <Toggle
   bind:isToggled
-  label="Whee"
+  label="With Style"
   style="--toggleBackgroundColor: orange"
 />
