a39c39f198ef408924c053d3cd95a751cd6e6d9b
spences10 <spences10apps@gmail.com>
2021-07-14 08:43:02 +0100

    feat: :sparkles: add reuseable field

---
+++ src/lib/field.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let label = ''
+  export let instructions = ''
+  export let style = ''
+  export let value
+  export let placeholder
+  export let type = 'text'
+
+  function handleInput(event) {
+    value = type.match(/^(number|range)$/)
+      ? +event.target.value
+      : event.target.value
+  }
+</script>
+
+<div {style}>
+  <label>
+    {#if label}
+      <span>
+        {label}
+      </span>
+      <br />
+    {/if}
+    {#if instructions}
+      <span class="instructions">
+        {instructions}
+      </span>
+      <br />
+    {/if}
+    <input on:input={handleInput} {type} {value} {placeholder} />
+  </label>
+</div>
+
+<style>
+  div {
+    margin-bottom: 1rem;
+  }
+  .instructions {
+    opacity: 0.7;
+    font-size: var(--instructionsFontSize, 14px);
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,28 @@
 <script>
   import Accordian from '$lib/accordian.svelte'
+  import Field from '$lib/field.svelte'
   import SearchFilter from '$lib/search-filter.svelte'
   import Toggle from '$lib/toggle.svelte'
   let isToggled = false
 
   let items = ['svelte', 'react', 'vue', 'angular']
-  let search
+  let search = ''
 </script>
 
 <h1>Welcome to SvelteKit UI</h1>
+
+<Field value={0} type="number" />
+
+<hr />
+
+<Field
+  bind:value={search}
+  label="Search..."
+  instructions="Type to search"
+  placeholder="Svelte"
+/>
+
+<hr />
 
 <SearchFilter {items} bind:search />
 
