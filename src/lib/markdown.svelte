685f21b9d4247589a3ae7d675f794f2768728163
spences10 <spences10apps@gmail.com>
2021-07-14 09:43:17 +0100

    feat: :sparkles: add markdown component

---
+++ src/lib/markdown.svelte
@@ -0,0 +1,24 @@
+<script>
+  import marked from 'marked'
+
+  let text = ''
+  export let markdownText = text
+  $: {
+    markdownText = marked(text)
+  }
+</script>
+
+<div class="wrapper">
+  <textarea id="" cols="30" rows="10" bind:value={text} />
+  <!-- 🚨 use DOM purify for unsanitised html 🚨 -->
+  <div>{@html markdownText}</div>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+  }
+  .wrapper > * {
+    width: 50%;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,27 @@
 <script>
   import Accordian from '$lib/accordian.svelte'
   import Field from '$lib/field.svelte'
+  import Markdown from '$lib/markdown.svelte'
   import SearchFilter from '$lib/search-filter.svelte'
   import Toggle from '$lib/toggle.svelte'
   let isToggled = false
 
   let items = ['svelte', 'react', 'vue', 'angular']
   let search = ''
+  let markdownText = ''
 </script>
 
 <h1>Welcome to SvelteKit UI</h1>
+
+<Markdown bind:markdownText />
+
+<hr />
+
+<p>Markdown text outside of the component</p>
+
+{@html markdownText}
+
+<hr />
 
 <Field value={0} type="number" />
 
