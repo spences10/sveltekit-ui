e67eff8e045a02797a841ec7f5a7cf54c3fbba5f
spences10 <spences10apps@gmail.com>
2021-07-15 07:51:39 +0100

    feat: :sparkles: add storybook

---
+++ src/lib/toggle.stories.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import Toggle from './toggle.svelte'
+</script>
+
+<Meta title="Toggle" component={Toggle} />
+
+<Story name="default">
+  <Toggle />
+</Story>
