ba42e9e526b982b4340306ca33d7d79fa40fec00
spences10 <spences10apps@gmail.com>
2021-07-15 09:06:25 +0100

    feat: :sparkles: add toggle story

--- src/lib/toggle.stories.svelte
+++ src/lib/toggle.stories.svelte
@@ -1,10 +1,14 @@
 <script>
-  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
   import Toggle from './toggle.svelte'
 </script>
 
 <Meta title="Toggle" component={Toggle} />
 
-<Story name="default">
-  <Toggle />
-</Story>
+<Template let:args>
+  <Toggle {...args} />
+</Template>
+
+<Story name="default" />
+<Story name="toggled" args={{ isToggled: true }} />
+<Story name="with label" args={{ label: 'Yo!' }} />
